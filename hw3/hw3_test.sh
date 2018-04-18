a5333f62ad0ad9188ca139d6872a3c2884fc06c7
r06921081 <r06921081@ntu.edu.tw>
2018-04-18 15:41:32 +0800

    name

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/6ua8py9tct6dhya/model.0279-0.7157.h5?dl=1
+wget https://www.dropbox.com/s/6ua8py9tct6dhya/model.0279-0.7157.h5
 python predict.py $1 $2 $3
