54cd4d136d71415d39bcd4de911a152714cd7945
r06921081 <r06921081@ntu.edu.tw>
2018-04-18 15:36:31 +0800

    use Dropbox

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/r06921081/ML2018SPRING/releases/download/0.0.0/model.0279-0.7157.h5
+wget https://www.dropbox.com/s/6ua8py9tct6dhya/model.0279-0.7157.h5?dl=1
 python predict.py $1 $2 $3
