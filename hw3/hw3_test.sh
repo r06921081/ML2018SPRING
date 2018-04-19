d75812ed2a7dd6f8134b03765bf96ebf16060a99
r06921081 <r06921081@ntu.edu.tw>
2018-04-19 18:07:37 +0800

    add skip

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/6ua8py9tct6dhya/first.h5
-wget https://www.dropbox.com/s/qsi069s9z9t2fgn/ensemble.h5
+if [ -f "./first.h5" ]; then
+    echo "File first.h5 exists, skip download."
+else
+    wget https://www.dropbox.com/s/6ua8py9tct6dhya/first.h5
+fi
+if [ -f "./ensemble.h5" ]; then
+    # 檔案 /path/to/dir/filename 存在
+    echo "File ensemble.h5 exists, skip download."
+else
+    wget https://www.dropbox.com/s/qsi069s9z9t2fgn/ensemble.h5
+fi
 python predict.py $1 $2 $3
