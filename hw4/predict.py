4541832ed0a6c298c065c97e538f7e4af1f01c3d
r06921081 <r06921081@ntu.edu.tw>
2018-05-01 20:48:35 +0800

    comment on it

--- hw4/predict.py
+++ hw4/predict.py
@@ -10,7 +10,11 @@ from sklearn.manifold import Isomap
 from tools import p
 import dataprocess as dp
 from sklearn.decomposition import PCA
-
+'''
+argv[1] train data
+argv[2] test data
+argv[3] result dir
+'''
 
 X = np.load(sys.argv[1])
 
@@ -24,56 +28,16 @@ model.summary()
 encoder = Model(inputs=model.get_layer('input_1').input,
                                  outputs=model.get_layer('d3').output)
 
-DL_input = Input(model.get_layer('d4').input_shape[1:])
-decoder = DL_input
+cnn_input = Input(model.get_layer('d4').input_shape[1:])
+decoder = cnn_input
 for layer in model.layers[9:]:
     decoder = layer(decoder)
-decoder = Model(inputs=DL_input, outputs=decoder)
+decoder = Model(inputs=cnn_input, outputs=decoder)
 
 encoded_imgs = encoder.predict(image[:10].reshape(-1,28,28,1))
 image_X_autoencoder = encoder.predict(image.reshape(-1,28,28,1))
 decoded_imgs = decoder.predict(encoded_imgs)
 
-
-# n = 10  # how many digits we will display
-# plt.figure(figsize=(20, 4))
-# for i in range(n):
-#     # display original
-#     ax = plt.subplot(2, n, i + 1)
-#     plt.imshow(image[i].reshape(28, 28))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-
-#     # display reconstruction
-#     ax = plt.subplot(2, n, i + 1 + n)
-#     plt.imshow(decoded_imgs[i].reshape(28, 28))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-
-
-# from sklearn.decomposition import PCA
-# pca = PCA(n_components=32, whiten=True,svd_solver="full")
-# image_X_PCA = pca.fit_transform(image)
-# decoded_imgs = pca.inverse_transform(image_X_PCA[:10])
-# n = 10  # how many digits we will display
-# plt.figure(figsize=(20, 4))
-# for i in range(n):
-#     # display original
-#     ax = plt.subplot(2, n, i + 1)
-#     plt.imshow(image[i].reshape(28, 28))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-
-#     # display reconstruction
-#     ax = plt.subplot(2, n, i + 1 + n)
-#     plt.imshow(decoded_imgs[i].reshape(28, 28))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-# plt.show()
 
 from sklearn.cluster import KMeans
 import pandas as pd
@@ -86,7 +50,7 @@ pca300.fit(image_X_autoencoder)
 ppa300 = pca300.fit_transform(image_X_autoencoder) 
 
 
-cluster_PCA= KMeans(n_clusters=2)
+cluster_PCA = KMeans(n_clusters=2)
 cluster_PCA.fit(ppa300)
 pt = cluster_PCA.fit_transform(ppa300)
 
