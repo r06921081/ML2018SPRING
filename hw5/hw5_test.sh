ffd6cee42a9b699d711cc1cdd4d296ac24408748
r06921081 <r06921081@ntu.edu.tw>
2018-05-29 22:06:00 +0800

    ddd

--- hw5/hw5_test.sh
+++ hw5/hw5_test.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+if [ -f "./ensemble.h5" ]; then
+    # 檔案 /path/to/dir/filename 存在
+    echo "File ensemble.h5 exists, skip download."
+else
+    wget https://www.dropbox.com/s/47336h56evcxzrg/ensemble.h5
+fi
 python predict.py $1 $2 $3
