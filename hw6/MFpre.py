0b43acd6b63e4d3f2ed137d08296bde254de2e66
r06921081 <r06921081@ntu.edu.tw>
2018-06-07 01:23:02 +0800

    MFpre.py modeldir set well

--- hw6/MFpre.py
+++ hw6/MFpre.py
@@ -8,7 +8,7 @@ mean = 3.58171208604
 
 testX = loadtest(sys.argv[1])
 print(testX.shape)
-model = load_model('./noupdate/0.85686/model.0.8507.h5', custom_objects={'rmse': normalize_rmse})
+model = load_model('./model.0.8507.h5', custom_objects={'rmse': normalize_rmse})
 
 result = model.predict(np.hsplit(testX, 2), batch_size= 1024)
 result = result * 1.116897661 + 3.581712086
